02102ba54ab8649fdd4c20dfd21e00118958d0b2
AplinXy9plin <aplinxy9plin@gmail.com>
2021-05-18 14:09:26 +0700

    Add files via upload

---
+++ poolwatch-clone/README.md
@@ -0,0 +1,2 @@
+# poolwatch-clone
+ 
---
+++ poolwatch-clone/data.js
@@ -0,0 +1,34 @@
+// officialProtocol: true - зеленый, false - красный
+// scamRisk: true - high, false - safe
+const jsonData = [
+  {
+    pool: {
+      name: "Google",
+      link: "https://google.com"
+    },
+    fee: 123,
+    networkHashrate: 1337,
+    officialProtocol: true,
+    scamRisk: true
+  },
+  {
+    pool: {
+      name: "Yandex",
+      link: "https://yandex.ru"
+    },
+    fee: 123,
+    networkHashrate: 1337,
+    officialProtocol: false,
+    scamRisk: false
+  },
+  {
+    pool: {
+      name: "Spotify",
+      link: "https://spotify.com"
+    },
+    fee: 123,
+    networkHashrate: 1337,
+    officialProtocol: true,
+    scamRisk: false
+  }
+];
